afe974aa359c6cb6db8a68095065d34322e00347
Greg-Goonewardene <83610107+Greg-Goonewardene@users.noreply.github.com>
2021-06-09 15:19:43 +0100

    Initial commit

---
+++ main.js
@@ -0,0 +1,15 @@
+const hamburger = document.querySelector('.hamburger');
+const topMenu = document.querySelector('.top_menu');
+const menuLinks = document.querySelectorAll('.top_menu li')
+
+hamburger.addEventListener ('click', () => {
+  topMenu.classList.toggle('open');
+});
+
+
+
+
+
+
+
+// Run function when page loads
